61e0a3738b6d01d84c6648d3f0e2c3003726b46a
luojiaquan7737 <58420846@qq.com>
2018-03-22 22:51:22 +0800

    Site updated: 2018-03-22 22:51:21

---
+++ js/src/dytitle.js
@@ -0,0 +1,1 @@
+var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",function(){document.hidden?($('[rel="icon"]').attr("href","/img/TEP.ico"),document.title=" 你不理我了！",clearTimeout(titleTime)):($('[rel="icon"]').attr("href","/favicon.ico"),document.title=" 么么哒 "+OriginTitile,titleTime=setTimeout(function(){document.title=OriginTitile},2e3))});
